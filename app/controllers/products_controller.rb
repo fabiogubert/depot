a96f8f709213caccaa6da562c88c2581adf7c7d0
Codemis <johnathan@still-water.com>
2012-05-16 21:00:13 -0700

    Completed chapter 7.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,7 +41,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(params[:product])
-
+		
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+	validates :description, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true, presence: true
+	validates :image_url, format: {
+		with: %r{\.(gif|jpg|png)$}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+	# validates :price, format: {
+	# 	with: %r{\d*\.\d\d\z},
+	# 	message: 'must be a valid dollar amount.'
+	# }
 end
