5c659db85163139b494753381ce6deb5efb1b725
Codemis <johnathan@still-water.com>
2012-05-23 20:53:12 -0700

    Finished ch 9.

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,17 @@ class Product < ActiveRecord::Base
 	# 	with: %r{\d*\.\d\d\z},
 	# 	message: 'must be a valid dollar amount.'
 	# }
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+	
+	private
+		# ensure that there are no line items referencing this product
+		def ensure_not_referenced_by_any_line_item
+		  if line_items.empty?
+		    return true
+		  else
+		    errors.add(:base, 'Line Items present')
+		    return false
+		  end
+		end
 end
